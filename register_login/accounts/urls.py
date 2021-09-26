3f478f976825975942bb91646b605ab1f57a19d2
nitish2022 <nitishmaheshwari11@gmail.com>
2021-09-26 10:56:54 +0530

    make public

---
+++ register_login/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.loginUser, name = 'index'),
+    path('home/', views.home, name = 'home'),
+    path('register/', views.registerUser, name = 'register'),
+    path('logout/', views.logoutUser, name = 'logout'),
+
+]
